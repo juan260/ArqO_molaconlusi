b1bdebb1d0bd5b3a56f43196594355d0c8660a76
Lucia Asencio Martin <luciaasen@gmail.com>
2017-12-07 18:39:52 +0100

    f

--- practica4/material/pescalar_par2.c
+++ practica4/material/pescalar_par2.c
@@ -3,17 +3,26 @@
 //
 #include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>
 #include "arqo4.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
 	float *A=NULL, *B=NULL;
-	long long k=0;
+	long long k=0, tam;
+	int nthreads;
 	struct timeval fin,ini;
 	float sum=0;
 	
-	A = generateVector(M);
-	B = generateVector(M);
+	if(argc != 3){
+		printf("Error en los arg entrada: ./pescalar_par2.c tamanio nthreads\n");
+		return -1;
+	}
+	
+	tam = atoi(argv[1]);
+	nthreads = atoi(argv[2]);
+	A = generateVector(tam);
+	B = generateVector(tam);
 	if ( !A || !B )
 	{
 		printf("Error when allocationg matrix\n");
@@ -25,16 +34,17 @@ int main(void)
 	gettimeofday(&ini,NULL);
 	/* Bloque de computo */
 	sum = 0;
+	omp_set_num_threads(nthreads);
 	#pragma omp parallel for reduction(+:sum)
-	for(k=0;k<M;k++)
+	for(k=0;k<tam;k++)
 	{
 		sum = sum + A[k]*B[k];
 	}
 	/* Fin del computo */
 	gettimeofday(&fin,NULL);
 
-	printf("Resultado: %f\n",sum);
-	printf("Tiempo: %f\n", ((fin.tv_sec*1000000+fin.tv_usec)-(ini.tv_sec*1000000+ini.tv_usec))*1.0/1000000.0);
+	//printf("Resultado: %f\n",sum);
+	printf("%f\n", ((fin.tv_sec*1000000+fin.tv_usec)-(ini.tv_sec*1000000+ini.tv_usec))*1.0/1000000.0);
 	freeVector(A);
 	freeVector(B);
 
